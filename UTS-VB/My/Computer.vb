1248b193598ee585476f4635d50464ce86f1fbcf
Rizky Aridlo Rivedo <83388769+RizzKG@users.noreply.github.com>
2022-11-20 08:35:49 +0700

    Add files via upload

---
+++ UTS-VB/My/Computer.vb
@@ -0,0 +1,7 @@
+﻿Namespace My
+    Friend Class Computer
+        Friend Shared Function FileSystem() As Object
+            Throw New NotImplementedException()
+        End Function
+    End Class
+End Namespace
